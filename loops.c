60d7f494e1a003be5c42dcacdcd17f743d3b812e
Keerti Yadav <keertiyadav065@gmail.com>
2026-01-07 20:39:59 +0530

    Add files via upload

---
+++ loops.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+//write a program to print first 10 even natural numbers
+int main(){
+    int i = 1;
+    while(i<=10)
+    {
+        printf("%d\n",2*i);
+        i++;
+    }
+    return 0;
+}
